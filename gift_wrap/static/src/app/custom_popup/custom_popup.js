69c7298c58d78a50f3a3de6b51ac0f1ccdd444ca
Akshay-cp7 <com.akshaycp@gmail.com>
2025-03-04 12:49:31 +0530

    changes

--- gift_wrap/static/src/app/custom_popup/custom_popup.js
+++ gift_wrap/static/src/app/custom_popup/custom_popup.js
@@ -1,22 +1,57 @@
-import { Component } from "@odoo/owl";
+import { Component, useState, onWillStart } from "@odoo/owl";
 import { Dialog } from "@web/core/dialog/dialog";
 import { patch } from "@web/core/utils/patch";
 import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
+import { useService } from "@web/core/utils/hooks";
 
-export class EmptyPopup extends Component {
-    static template = "pos_custom.EmptyPopup";
+export class GiftWrapPopup extends Component {
+    static template = "pos_custom.GiftWrapPopup";
     static components = { Dialog };
 
-    close() {
-        this.props.close();
+    setup() {
+        this.orm = useService("orm");
+        this.state = useState({ giftWrapProducts: [] });
 
+        onWillStart(async () => {
+            const products = await this.orm.searchRead(
+                "product.product",
+                [["gift_wrap", "=", true]],  
+                ["id", "name", "lst_price", "image_128"]
+            );
+            this.state.giftWrapProducts = products;
+        });
+    }
+
+    selectGiftWrap(product) {
+        if (this.props.addGiftWrapToOrder) {
+            this.props.addGiftWrapToOrder(product);
+        }
+        this.close();
+    }
+
+    close() {
+        if (this.props.close) {
+            this.props.close();
+        }
     }
 }
+
 patch(ControlButtons.prototype, {
-    onClickPopupSingleField() {
-        this.dialog.add(EmptyPopup, {
-            title: "Popup",
+    async onClickPopupGiftWrap() {
+        this.dialog.add(GiftWrapPopup, {
+            props: { title: "Select Gift Wrap" },  // ✅ Ensure title is passed as a prop
+            addGiftWrapToOrder: this.addGiftWrapToOrder.bind(this),
             close: () => this.dialog.close(),
         });
+        
+    },
+
+    addGiftWrapToOrder(product) {
+        const order = this.env.services.pos.get_order();
+        if (order) {
+            order.add_product(this.env.services.pos.db.get_product_by_id(product.id), {
+                price: product.lst_price,
+            });
+        }
     },
 });
